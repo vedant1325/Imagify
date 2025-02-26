cc26d562dcbe7361dfaa59878ca23a86037315a8
Vedant Dange <vedantdange149@gmail.com>
2025-02-26 12:27:20 +0530

    added bcryptjs

--- server/contollers/userContollers.js
+++ server/contollers/userContollers.js
@@ -1,5 +1,5 @@
 import userModel from "../Models/userModel.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 
 export const registerUser = async (req, res) => {
